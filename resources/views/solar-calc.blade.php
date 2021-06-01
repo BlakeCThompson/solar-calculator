8b7d8b59535358fd73ce7b40caa45f52202b3d29
blakethompson <blake.thompson@ascentdesk.com>
2021-06-01 00:11:52 -0600

    initial commit with solar logic

---
+++ resources/views/solar-calc.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <body>
+        <title>Laravel</title>
+        <form>
+            <div><tag>Monthly kWh you produced: </tag><input id="kwh"></div>
+            <div><tag>kWh that you bought: </tag><input></div>
+        </form>
+
+        </div>
+    </body>
+</html>
